c812bcd1e1d5c55b403bdb13c2efc533383a5494
Nic Sevic <nasevic@gmail.com>
2019-03-19 09:46:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function doToElementsInArray(array, callback) {
   array.forEach(callback)
 }
 
-function changeCompletely(array, callback) {
-  array.forEach(callback)
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
 }
